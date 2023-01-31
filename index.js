c835da24a2780a485f46bdc87a0a402ee3a3fed5
Logesh kumar <phoenix.logu@gmail.com>
2023-01-31 15:35:21 +0530

    fix param

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {  
-  const fontColor = core.getInput("font-color");
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
