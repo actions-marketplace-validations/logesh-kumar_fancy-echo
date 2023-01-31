fef97f8471c3ea32e70ae9a74cc96d5694bb8ff0
Logesh kumar <phoenix.logu@gmail.com>
2023-01-31 19:08:37 +0530

    add chalk

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from "@actions/core";
+import chalk from "chalk";
+
+//const github = require("@actions/github");
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
+  console.log(chalk.blue.bgRed.bold(`Hello ${nameToGreet}!`));
   const time = new Date().toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
