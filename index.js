6009beb345f7f597e01c488cf6e129ddc4adccaf
Logesh kumar <phoenix.logu@gmail.com>
2023-01-31 15:12:48 +0530

    add branding

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
+try {  
+  const fontColor = core.getInput("font-color");
   console.log(`Hello ${nameToGreet}!`);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
